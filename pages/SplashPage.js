a0f90f44e901d8aaf4187b99f032ea53eb8f832e
fooledyouonce <64298603+fooledyouonce@users.noreply.github.com>
2021-11-28 08:17:18 -0800

    added scrollview

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -1,49 +1,58 @@
 // Example: Example of SQLite Database in React Native
 // https://aboutreact.com/example-of-sqlite-database-in-react-native
 
-import React, { useEffect } from 'react';
-import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native';
-import Mybutton from './components/Mybutton';
-import Mytext from './components/Mytext';
-import { openDatabase } from 'react-native-sqlite-storage';
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StyleSheet,
+  ScrollView
+} from "react-native";
+import Mybutton from "./components/Mybutton";
+import Mytext from "./components/Mytext";
+import { openDatabase } from "react-native-sqlite-storage";
 
-var db = openDatabase({ name: 'UserDatabase.db' });
+var db = openDatabase({ name: "UserDatabase.db" });
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    paddingTop: 50
   },
-  
+
   logo: {
-    marginRight:'auto',
-    marginLeft:'auto',
+    marginRight: "auto",
+    marginLeft: "auto",
     marginTop: 35,
     marginBottom: 50
-  },
+  }
 });
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, backgroundColor: '#055C9D' }}>
-      <View style={{ flex: 1 }}>
-        <Image
-          style={styles.logo}
-          source={require('./images/Tuffy.png')}
-        />
+    <View style={{ flex: 1, backgroundColor: "#055C9D" }}>
+      <ScrollView style={styles.scrollview}>
+        <Image style={styles.logo} source={require("./images/Tuffy.png")} />
 
         <Mybutton
           title="Create Account"
-          customClick={() => navigation.navigate('CreateScreen')}
+          customClick={() => navigation.navigate("CreateScreen")}
         />
         <Mybutton
           title="Login"
-          customClick={() => navigation.navigate('LoginScreen')}
+          customClick={() => navigation.navigate("LoginScreen")}
         />
         <Mybutton
           title="Forgot Account Info"
-          customClick={() => navigation.navigate('ForgotPassword')}
+          customClick={() => navigation.navigate("ForgotPassword")}
         />
-      </View>
+        <Mybutton
+          //delete block once done linking on homepage
+          title="Profile Page Info"
+          customClick={() => navigation.navigate("Profile")}
+        />
+      </ScrollView>
     </View>
   );
 };
