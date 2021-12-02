eeb71dcfbd3c9cab02d7840180554edb0960afd7
fooledyouonce <64298603+fooledyouonce@users.noreply.github.com>
2021-12-01 23:10:14 -0800

    Update SplashPage.js

--- pages/SplashPage.js
+++ pages/SplashPage.js
@@ -2,19 +2,12 @@
 // https://aboutreact.com/example-of-sqlite-database-in-react-native
 
 import React, { useEffect } from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  StyleSheet,
-  ScrollView
-} from "react-native";
+import { View, Text, SafeAreaView, Image, StyleSheet, ScrollView} from 'react-native';
 import Mybutton from "./components/Mybutton";
 import Mytext from "./components/Mytext";
 import { openDatabase } from "react-native-sqlite-storage";
 
-var db = openDatabase({ name: "UserDatabase.db" });
+var db = openDatabase({ name: "Default.db" });
 
 const styles = StyleSheet.create({
   container: {
@@ -30,28 +23,45 @@ const styles = StyleSheet.create({
 });
 
 const HomeScreen = ({ navigation }) => {
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Users'",
+        [],
+        function (tx, res) {
+          console.log('item:', res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Users', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS Users(ID INTEGER PRIMARY KEY AUTOINCREMENT, first_name VARCHAR(20), last_name VARCHAR(20), cwid INT(9),password VARCHAR(20))',
+              []
+            );
+          }
+        }
+      );
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1, backgroundColor: "#055C9D" }}>
       <ScrollView style={styles.scrollview}>
-        <Image style={styles.logo} source={require("./images/Tuffy.png")} />
+      <Image
+            style={styles.logo}
+            source={require('./images/Tuffy.png')}
+          />
 
-        <Mybutton
-          title="Create Account"
-          customClick={() => navigation.navigate("CreateScreen")}
-        />
-        <Mybutton
-          title="Login"
-          customClick={() => navigation.navigate("LoginScreen")}
-        />
-        <Mybutton
-          title="Forgot Account Info"
-          customClick={() => navigation.navigate("ForgotPassword")}
-        />
-        <Mybutton
-          //delete block once done linking on homepage
-          title="Profile Page Info"
-          customClick={() => navigation.navigate("Profile")}
-        />
+          <Mybutton
+            title="Create Account"
+            customClick={() => navigation.navigate('CreateScreen')}
+          />
+          <Mybutton
+            title="Login"
+            customClick={() => navigation.navigate('LoginScreen')}
+          />
+          <Mybutton
+            title="Forgot Account Info"
+            customClick={() => navigation.navigate('ForgotPassword')}
+          />
       </ScrollView>
     </View>
   );
